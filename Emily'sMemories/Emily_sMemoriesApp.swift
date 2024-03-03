ee216ac045567d064e14281fc79312370ec98e19
EmLiu0314 <emilyliu52106@gmail.com>
2024-03-03 00:34:42 -0600

    Initial Commit

---
+++ Emily'sMemories/Emily_sMemoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emily_sMemoriesApp.swift
+//  Emily'sMemories
+//
+//  Created by Liu, Emily on 3/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Emily_sMemoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Emily'sMemoriesUITests/Emily_sMemoriesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Emily_sMemoriesUITestsLaunchTests.swift
+//  Emily'sMemoriesUITests
+//
+//  Created by Liu, Emily on 3/3/24.
+//
+
+import XCTest
+
+final class Emily_sMemoriesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
